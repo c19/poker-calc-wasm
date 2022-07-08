c39487d9a9f6a57c8c1bd507f55f75dc8bf77bf0
c19 <classone2010@gmail.com>
2022-07-08 23:40:34 +0800

    add wasm binding

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 mod utils;
 
+use poker_calc::{card::Card, winrate};
 use wasm_bindgen::prelude::*;
 
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
@@ -9,11 +10,28 @@ use wasm_bindgen::prelude::*;
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
 #[wasm_bindgen]
-extern {
+extern "C" {
     fn alert(s: &str);
 }
 
 #[wasm_bindgen]
 pub fn greet() {
-    alert("Hello, {{project-name}}!");
+    alert("Hello, poker-calc!");
+}
+
+#[wasm_bindgen]
+pub fn calc(hand_str: String, board_str: String) -> f64 {
+    let (left, right) = hand_str.split_at(2);
+    let hand = [Card::new(left), Card::new(right)];
+    let mut board = Vec::new();
+    if board_str.chars().count() == 10 {
+        board.push(Card::new(&board_str[0..2]));
+        board.push(Card::new(&board_str[2..4]));
+        board.push(Card::new(&board_str[4..8]));
+        board.push(Card::new(&board_str[8..10]));
+    }
+    let mut rng = fastrand::Rng::new();
+    let result = winrate::calc(&mut rng, hand, &board, 6, 1000000, false);
+    println!("{}", result);
+    return result;
 }
